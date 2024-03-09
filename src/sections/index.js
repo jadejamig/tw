5397028047229c2d368e62c4b327e74681a2ed22
Anselm Jade Jamig <jadejamigdev@gmail.com>
2024-03-09 11:18:51 +0800

    nav and hero

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import CustomerReview from './CustomerReview';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    CustomerReview,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe
+}
